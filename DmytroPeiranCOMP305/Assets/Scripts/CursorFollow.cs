fbf7c261e54f63fedc0ebf694707cf5b2ef14155
qwe3804132 <liupeiran9324@outlook.com>
2019-04-19 21:52:05 -0400

    Initial commit

---
+++ DmytroPeiranCOMP305/Assets/Scripts/CursorFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
---
+++ DmytroPeiranCOMP305/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health;
+    [HideInInspector]
+    public Transform player;
+
+    public float speed;
+    public float timeBetweenAttacks;
+    public int damage;
+
+    public GameObject deathEffect;
+
+    public virtual void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void TakeDamage (int amount) {
+        health -= amount;
+        if (health <= 0)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
+    }
+	
+}
